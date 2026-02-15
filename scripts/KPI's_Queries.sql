b322a970d71d4801b6fd14813df5b2284dc2a627
Anjaneya <anjaneyab3.142@gmail.com>
2026-02-15 12:28:17 +0530

    Updated dashboard, SQL queries, and added project report

--- scripts/KPI's_Queries.sql
+++ scripts/KPI's_Queries.sql
@@ -18,3 +18,70 @@ CAST(COUNT(DISTINCT order_id) AS DECIMAL(10,2)) AS DECIMAL(10,2))
 AS Avg_Pizzas_per_order
 FROM pizza_sales
  
+
+ 
+----------------- Month Extraction FUNCTIONS--------
+--1. For Month as Integer (1–12):
+-- Use the MONTH function:
+
+SELECT 
+    order_date,
+    MONTH(order_date) AS month
+FROM pizza_sales;
+
+SELECT 
+    order_date,
+    DATEPART(month, order_date) AS month
+FROM pizza_sales;
+
+-- 2. For Month Name (e.g., "January", "February"):
+--Use FORMAT:
+
+SELECT 
+    order_date,
+    FORMAT(order_date, 'MMMM') AS month_name
+FROM pizza_sales;
+
+--3. For Month Abbreviation (e.g., "Jan", "Feb"):
+-- Use FORMAT:
+SELECT 
+    order_date,
+    FORMAT(order_date, 'MMM') AS month_abbr
+FROM pizza_sales;
+
+---------------- Year Extraction-------------------
+
+/*Purpose	Function	Example Output	Best For
+Year as integer	YEAR(date_column)	2025	Calculations, grouping
+Year as integer	DATEPART(year, date_column)	2025	Same as above
+Year as string	FORMAT(date_column, 'yyyy')	"2025"	Display only*/
+
+-- Using the YEAR() Function ----
+SELECT 
+    order_date,
+    YEAR(order_date) AS year
+FROM pizza_sales;
+
+
+
+-- Using DATEPART() ---
+SELECT 
+    order_date,
+    DATEPART(year, order_date) AS year
+FROM pizza_sales;
+
+-- 1) Using FORMAT() (As a String) ---
+SELECT 
+    order_date,
+    FORMAT(order_date, 'yyyy') AS year_str
+FROM pizza_sales;
+
+
+-- Add a new column for the month abbreviation
+ALTER TABLE pizza_sales ADD month VARCHAR(3);
+
+-- Populate it with the 3-letter month abbreviation
+UPDATE pizza_sales SET month = FORMAT(order_date, 'MMM');
+
+
+
